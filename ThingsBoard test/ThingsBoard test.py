d85efd8b53991eb85400ff444007a012f0792cdc
keerthanchand <51761160+keerthanchand@users.noreply.github.com>
2019-09-23 01:06:07 +0530

    Add files via upload

---
+++ ThingsBoard test/ThingsBoard test.py
@@ -0,0 +1,35 @@
+import paho.mqtt.client as paho
+import os
+import json
+import time
+import random
+from datetime import datetime
+
+ACCESS_TOKEN = '9QgFqcwTsfpe1fYUtnRQ'
+broker = "demo.thingsboard.io"
+port = 1883
+
+
+def on_publish(client, userdata, result):
+    print("data published to thingsboard \n")
+    pass
+
+
+client1 = paho.Client("control1")
+client1.on_publish = on_publish
+client1.username_pw_set(ACCESS_TOKEN)
+client1.connect(broker, port, keepalive=60)
+while True:
+    n = random.randint(1,50)
+    k = random.randint(1,40)
+    payload = "{"
+    payload += "\"Humidity\":";
+    payload += str(n);
+    payload += ",";
+    payload += "\"Temperature\":";
+    payload += str(k);
+    payload += "}"
+    ret = client1.publish("v1/devices/me/telemetry", payload)
+    print("Please check LATEST TELEMETRY field of your device")
+    print(payload);
+    time.sleep(3)
